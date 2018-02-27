311632f4493d173650349d19ad8d268e91eb955a
wynne <664966415@qq.com>
2018-02-27 21:09:45 +0800

    init framwork

---
+++ app/src/main/java/com/wynne/knowledge/tree/InitContent.java
@@ -0,0 +1,19 @@
+package com.wynne.knowledge.tree;
+
+/**
+ * @author XXW
+ * @date 2018/2/27
+ */
+
+public interface InitContent {
+
+    /**
+     * 初始化View
+     */
+    void initView();
+
+    /**
+     * 初始化数据
+     */
+    void initDate();
+}
--- app/src/main/java/com/wynne/knowledge/tree/MainActivity.java
+++ app/src/main/java/com/wynne/knowledge/tree/MainActivity.java
@@ -1,16 +1,64 @@
 package com.wynne.knowledge.tree;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author XXW
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InitContent {
+    private RecyclerView mRvContent;
+    private String[] mStrings = {};
+    private List<String> mListContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initDate();
+        initView();
     }
+
+    @Override
+    public void initView() {
+        mRvContent = (RecyclerView) findViewById(R.id.rv_content);
+    }
+
+    @Override
+    public void initDate() {
+        mListContent = Arrays.asList(mStrings);
+    }
+
+
+    public class RvAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
 }
