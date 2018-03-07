84cc9a0c7eaac07213ddf6f27a6f46802a3f54f6
wynne <664966415@qq.com>
2018-03-07 22:11:07 +0800

    notification create

---
+++ app/src/main/java/com/wynne/knowledge/tree/guide/NotificationPendActivity.java
@@ -0,0 +1,22 @@
+package com.wynne.knowledge.tree.guide;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wynne.knowledge.tree.R;
+
+/**
+ *
+ * @author XXW
+ * @date 2018/3/7
+ */
+
+public class NotificationPendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_pend_layout);
+    }
+}
