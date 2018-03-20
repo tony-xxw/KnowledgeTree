2653657d0fb511d7b6d6826deb4673408e6067cd
Wynne <a664966415@gmail.com>
2018-03-21 00:21:33 +0800

    messenger with bind

---
+++ app/src/main/java/com/wynne/knowledge/tree/guide/service/BindService.java
@@ -0,0 +1,67 @@
+package com.wynne.knowledge.tree.guide.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import java.util.Random;
+
+/**
+ * 1.同进程的Binder
+ * 2.Messenger
+ * 3.AIDL
+ *
+ * @author Wynne
+ * @date 2018/3/20
+ */
+
+public class BindService extends Service {
+    private final Binder mBinder = new LocalBinder();
+    private final Random mRandom = new Random();
+    static final int MSG_SAY_HELLO = 1;
+
+
+    private class LocalHandle extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SAY_HELLO:
+                    Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+
+        }
+    }
+
+    @Nullable
+    @Override
+
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+
+        return message.getBinder();
+    }
+
+
+    class LocalBinder extends Binder {
+
+        BindService getService() {
+            return BindService.this;
+        }
+    }
+
+
+    public int getRandomNumber() {
+        return mRandom.nextInt(100);
+    }
+
+    private Messenger message = new Messenger(new LocalHandle());
+}
--- app/src/main/java/com/wynne/knowledge/tree/guide/service/ServiceActivity.java
+++ app/src/main/java/com/wynne/knowledge/tree/guide/service/ServiceActivity.java
@@ -1,10 +1,18 @@
 package com.wynne.knowledge.tree.guide.service;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.wynne.knowledge.tree.R;
 
@@ -17,14 +25,26 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class ServiceActivity extends AppCompatActivity {
     private Intent intent;
+    private Intent bindIntent;
+    private BindService bindService;
+    private boolean mBinder = false;
+    private Messenger mMessenger;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.service_activity);
         intent = new Intent(this, StandardService.class);
+        bindIntent = new Intent(this, BindService.class);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        bindService(bindIntent, connection, BIND_AUTO_CREATE);
+        bindService(new Intent(this, BindService.class), messager,
+                BIND_AUTO_CREATE);
+    }
 
     public void onClick(View view) {
         switch (view.getId()) {
@@ -36,11 +56,65 @@ public class ServiceActivity extends AppCompatActivity {
                 stopService(intent);
                 break;
             case R.id.btn_bind:
+                if (mBinder) {
+//                    Toast.makeText(this, bindService.getRandomNumber() + "", Toast.LENGTH_LONG).show();
+                    sayHello(view);
+                }
                 break;
             case R.id.btn_unbind:
                 break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBinder) {
+            unbindService(connection);
+            mBinder = false;
+        }
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BindService.LocalBinder localBinder = (BindService.LocalBinder) service;
+            localBinder.getService();
+            bindService = localBinder.getService();
+            mBinder = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBinder = false;
+        }
+    };
+
+    private ServiceConnection messager = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mMessenger = new Messenger(service);
+            mBinder = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mMessenger = null;
+            mBinder = false;
+        }
+    };
+
+    public void sayHello(View v) {
+        if (!mBinder) {
+            return;
+        }
+        Message msg = Message.obtain(null, BindService.MSG_SAY_HELLO, 0, 0);
+        try {
+            mMessenger.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 }
