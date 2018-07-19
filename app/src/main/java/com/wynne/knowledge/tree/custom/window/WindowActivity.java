f0de6af4948e05168997e49567b1285e227e6902
wynne <664966415@qq.com>
2018-07-19 15:33:21 +0800

    Rulate

--- app/src/main/java/com/wynne/knowledge/tree/custom/window/WindowActivity.java
+++ app/src/main/java/com/wynne/knowledge/tree/custom/window/WindowActivity.java
@@ -1,8 +1,10 @@
 package com.wynne.knowledge.tree.custom.window;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.wynne.knowledge.tree.R;
 
@@ -12,10 +14,19 @@ import com.wynne.knowledge.tree.R;
  */
 
 public class WindowActivity extends AppCompatActivity {
+    Button mBtnWindow;
+    Drawable drawable;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.window_activity);
+        mBtnWindow = (Button) findViewById(R.id.btn_wd);
+
+
+        drawable = getResources().getDrawable(R.drawable.star_light);
+        drawable.setBounds(100, 10, 100, 50);
+
+        mBtnWindow.setBackground(drawable);
     }
 }
