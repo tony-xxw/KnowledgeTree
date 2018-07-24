a506054cc0fe8cf63465b26b77a7e935835f749d
Wynne <a664966415@gmail.com>
2018-07-24 23:17:00 +0800

    window

--- app/src/main/java/com/wynne/knowledge/tree/custom/window/WindowActivity.java
+++ app/src/main/java/com/wynne/knowledge/tree/custom/window/WindowActivity.java
@@ -1,10 +1,18 @@
 package com.wynne.knowledge.tree.custom.window;
 
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.wynne.knowledge.tree.R;
 
@@ -13,20 +21,76 @@ import com.wynne.knowledge.tree.R;
  * @date 2018/7/18
  */
 
-public class WindowActivity extends AppCompatActivity {
-    Button mBtnWindow;
-    Drawable drawable;
+public class WindowActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener {
+    Button mCreateWindowButton;
+    Button mFloatingButton;
+    private WindowManager.LayoutParams mLayoutParams;
+    private WindowManager manager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.window_activity);
-        mBtnWindow = (Button) findViewById(R.id.btn_wd);
+        mCreateWindowButton = (Button) findViewById(R.id.btn_wd);
+        mCreateWindowButton.setOnClickListener(this);
+
+        manager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+    }
 
 
-        drawable = getResources().getDrawable(R.drawable.star_light);
-        drawable.setBounds(100, 10, 100, 50);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults != null && grantResults.length > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    initWindow();
+                } else {
+                    Toast.makeText(WindowActivity.this, "拒绝权限", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
-        mBtnWindow.setBackground(drawable);
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                mLayoutParams.x = rawX;
+                mLayoutParams.y = rawY;
+                manager.updateViewLayout(mFloatingButton, mLayoutParams);
+                break;
+            default:
+                break;
+
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_wd:
+                mFloatingButton = new Button(this);
+                mFloatingButton.setText("click me");
+                mLayoutParams = new WindowManager.LayoutParams(
+                        WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, 0, 0,
+                        PixelFormat.TRANSPARENT);
+                mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+                mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+                mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+                mLayoutParams.x = 100;
+                mLayoutParams.y = 300;
+                mFloatingButton.setOnTouchListener(this);
+                manager.addView(mFloatingButton, mLayoutParams);
+                break;
+            default:
+                break;
+        }
     }
 }
