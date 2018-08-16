703d60d72706e4cf8d2e6e68df51125b3c71b373
wynne <a664966415@gmail.com>
2018-08-16 15:34:10 +0800

    glide

---
+++ app/src/main/java/com/wynne/knowledge/tree/base/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.wynne.knowledge.tree.base;
+
+import android.app.Application;
+import android.content.Context;
+
+public class BaseApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+}
--- app/src/main/java/com/wynne/knowledge/tree/bookmark/activity/BitmapActivity.java
+++ app/src/main/java/com/wynne/knowledge/tree/bookmark/activity/BitmapActivity.java
@@ -1,24 +1,35 @@
 package com.wynne.knowledge.tree.bookmark.activity;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.wynne.knowledge.tree.R;
 import com.wynne.knowledge.tree.base.BaseActivity;
+
+import java.lang.ref.WeakReference;
 
 /**
  * @author kf-010
  */
 public class BitmapActivity extends BaseActivity {
     ImageView mBitmapIv;
+    final MyHandler handler = new MyHandler(BitmapActivity.this);
+
 
     @Override
     public void initView() {
         mBitmapIv = findViewById(R.id.iv_bitmap);
         mBitmapIv.setImageBitmap(getBitmap(100, 100));
+
+        Glide.with(this).load(R.drawable.icon_large).into(mBitmapIv);
     }
+
 
     private Bitmap getBitmap(int requestX, int requestY) {
         Bitmap bitmapFactory = BitmapFactory.decodeResource(getResources(), R.drawable.icon_large);
@@ -60,5 +71,22 @@ public class BitmapActivity extends BaseActivity {
     @Override
     public int getLayoutId() {
         return R.layout.bitmap_activity;
+    }
+
+
+    static class MyHandler extends Handler {
+
+        public MyHandler(BitmapActivity activity) {
+            this.weakReference = new WeakReference<>(activity);
+        }
+
+        WeakReference<BitmapActivity> weakReference;
+
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            BitmapActivity activity = weakReference.get();
+        }
     }
 }
