e42ea86c753bd0320aa1d2fc0cc7baef73a5a6e5
Wynne <a664966415@gmail.com>
2018-09-25 23:07:02 +0800

    sideBar

---
+++ app/src/main/java/com/wynne/knowledge/tree/widget/SideBar.java
@@ -0,0 +1,27 @@
+package com.wynne.knowledge.tree.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author Wynne
+ * @date 2018/9/25
+ */
+
+public class SideBar extends View {
+    private Context mContext;
+
+    public SideBar(Context context) {
+        super(context);
+    }
+
+    public SideBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SideBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
