740ae78084be1d2a8d25b4edaa295bcb403b7d42
Wynne <a664966415@gmail.com>
2018-10-26 00:02:28 +0800

    Path Measure

--- app/src/main/java/com/wynne/knowledge/tree/bookmark/BookMarkFragment.java
+++ app/src/main/java/com/wynne/knowledge/tree/bookmark/BookMarkFragment.java
@@ -12,6 +12,7 @@ import com.wynne.knowledge.tree.bookmark.activity.DrawableActivity;
 import com.wynne.knowledge.tree.bookmark.activity.HandlerActivity;
 import com.wynne.knowledge.tree.bookmark.activity.TaskActivity;
 import com.wynne.knowledge.tree.bookmark.activity.ThreadActivity;
+import com.wynne.knowledge.tree.custom.book.AnimationPathActivity;
 import com.wynne.knowledge.tree.custom.book.CustomAnimationActivity;
 
 /**
@@ -70,6 +71,9 @@ public class BookMarkFragment extends BaseFragment implements View.OnClickListen
             case R.id.btn_anim_custom:
                 startActivity(new Intent(getActivity(), CustomAnimationActivity.class));
                 break;
+            case R.id.btn_anim_path:
+                startActivity(new Intent(getActivity(), AnimationPathActivity.class));
+                break;
             default:
                 break;
         }
@@ -85,6 +89,7 @@ public class BookMarkFragment extends BaseFragment implements View.OnClickListen
         mContentView.findViewById(R.id.btn_drawable).setOnClickListener(this::onClick);
         mContentView.findViewById(R.id.btn_anim).setOnClickListener(this::onClick);
         mContentView.findViewById(R.id.btn_anim_custom).setOnClickListener(this::onClick);
+        mContentView.findViewById(R.id.btn_anim_path).setOnClickListener(this::onClick);
     }
 
     @Override
---
+++ app/src/main/java/com/wynne/knowledge/tree/custom/book/AnimationPathActivity.java
@@ -0,0 +1,21 @@
+package com.wynne.knowledge.tree.custom.book;
+
+import com.wynne.knowledge.tree.R;
+import com.wynne.knowledge.tree.base.BaseActivity;
+
+/**
+ * @author Wynne
+ * @date 2018/10/25
+ */
+
+public class AnimationPathActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.animation_path_activity;
+    }
+}
