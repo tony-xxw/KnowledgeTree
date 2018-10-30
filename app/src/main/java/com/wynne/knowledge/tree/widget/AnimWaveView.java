f86db7f3b68b78f21ece5f7920b0a3f838b89836
Wynne <a664966415@gmail.com>
2018-10-31 00:19:11 +0800

    贝及埃曲线

---
+++ app/src/main/java/com/wynne/knowledge/tree/widget/AnimWaveView.java
@@ -0,0 +1,69 @@
+package com.wynne.knowledge.tree.widget;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * @author Wynne
+ * @date 2018/10/30
+ */
+
+public class AnimWaveView extends View {
+    private Paint mPaint;
+    private Path mPath;
+    private int mItemWaveLength = 600;
+    private int dx;
+
+
+    public AnimWaveView(Context context) {
+        super(context);
+    }
+
+    public AnimWaveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+        mPath = new Path();
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        startAnimations();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int originY = 300;
+        int halfWaveLen = mItemWaveLength / 2;
+        mPath.moveTo(-mItemWaveLength + dx, originY);
+        for (int i = -mItemWaveLength; i < getWidth() + mItemWaveLength; i += mItemWaveLength) {
+            mPath.rQuadTo(halfWaveLen / 2, -100, halfWaveLen, 0);
+            mPath.rQuadTo(halfWaveLen / 2, 100, halfWaveLen, 0);
+        }
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+
+    public void startAnimations() {
+        ValueAnimator animator = ValueAnimator.ofInt(0, mItemWaveLength);
+        animator.setDuration(2000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                dx = (int) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        animator.start();
+    }
+}
---
+++ app/src/main/java/com/wynne/knowledge/tree/widget/QuadView.java
@@ -0,0 +1,80 @@
+package com.wynne.knowledge.tree.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * @author Wynne
+ * @date 2018/10/30
+ */
+
+public class QuadView extends View {
+    private Paint mPaint;
+    private Path path = new Path();
+    private float x, y;
+
+    public QuadView(Context context) {
+        super(context);
+        init();
+    }
+
+    public QuadView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public QuadView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//
+//        path.moveTo(100, 300);
+//        path.quadTo(200, 200, 300, 300);
+//        path.quadTo(400, 400, 500, 300);
+//
+        canvas.drawColor(Color.WHITE);
+        canvas.drawPath(path, mPaint);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x = event.getX();
+                y = event.getY();
+                path.moveTo(x, y);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float endx = (x + event.getX()) / 2;
+                float endy = (y + event.getY()) / 2;
+                path.quadTo(x, y, endx, endy);
+                x = event.getX();
+                y = event.getY();
+                postInvalidate();
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
