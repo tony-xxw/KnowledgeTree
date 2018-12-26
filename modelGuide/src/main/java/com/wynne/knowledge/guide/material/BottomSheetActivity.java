2791a4f91257b97f686d2c5b8d415c74db093fc5
Wynne <a664966415@gmail.com>
2018-12-26 22:19:00 +0800

    测试 bottom Sheet recycleView结合

--- modelGuide/src/main/java/com/wynne/knowledge/guide/material/BottomSheetActivity.java
+++ modelGuide/src/main/java/com/wynne/knowledge/guide/material/BottomSheetActivity.java
@@ -1,21 +1,47 @@
 package com.wynne.knowledge.guide.material;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.wynne.knowledge.base.base.BaseActivity;
 import com.wynne.knowledge.guide.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author xxw
  */
 public class BottomSheetActivity extends BaseActivity {
     BottomSheetBehavior sheetBehavior;
+    RecyclerView recyclerView;
+
+    private String[] arrys = {
+            "Item1", "Item2", "Item3", "Item4",
+            "Item5", "Item6", "Item7", "Item8",
+            "Item9", "Item10", "Item11", "Item12",
+            "Item13", "Item14", "Item15", "Item16",
+            "Item17", "Item18", "Item19", "Item20"
+    };
+    private List<String> list;
 
     @Override
     public void initView() {
+        recyclerView = findViewById(R.id.rv_list);
+        list = Arrays.asList(arrys);
+        BottomAdapter adapter = new BottomAdapter();
+        LinearLayoutManager linearLayout = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayout);
+        recyclerView.setAdapter(adapter);
 
         sheetBehavior = BottomSheetBehavior.from(findViewById(R.id.cl_bottomSheet));
         sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
@@ -64,5 +90,36 @@ public class BottomSheetActivity extends BaseActivity {
     @Override
     public int getLayoutId() {
         return R.layout.bottom_activity;
+    }
+
+
+    public class BottomAdapter extends RecyclerView.Adapter<BottomAdapter.ViewHolder> {
+
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(BottomSheetActivity.BottomAdapter.ViewHolder holder, int position) {
+            holder.mTextView.setText(list.get(position) + " === " + position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            private TextView mTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView) itemView.findViewById(R.id.tv_item);
+            }
+        }
     }
 }
