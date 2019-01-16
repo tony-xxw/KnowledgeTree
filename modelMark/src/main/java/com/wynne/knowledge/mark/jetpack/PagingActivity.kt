1f1d085b60e4e8685c1b197e960087cf00e92657
Wynne <a664966415@gmail.com>
2019-01-16 22:51:26 +0800

    restore normal

--- modelMark/src/main/java/com/wynne/knowledge/mark/jetpack/PagingActivity.kt
+++ modelMark/src/main/java/com/wynne/knowledge/mark/jetpack/PagingActivity.kt
@@ -4,8 +4,6 @@ import com.wynne.knowledge.base.base.BaseActivity
 import com.wynne.knowledge.mark.R
 
 class PagingActivity : BaseActivity() {
-    val asc = Array(5, { i -> (i * i).toString() })
-    var mList: List<String>? = null
 
     override fun getLayoutId(): Int = R.layout.paging_activity
 
@@ -13,7 +11,5 @@ class PagingActivity : BaseActivity() {
 
     }
 
-    fun handleData() {
 
-    }
 }
