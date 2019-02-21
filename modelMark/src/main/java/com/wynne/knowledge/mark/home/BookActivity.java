0eb51b257d94eb602864ac7ef0ac3a770a348ee7
tony-xxw <a664966415@gmail.com>
2019-02-21 22:56:48 +0800

    String知识点

--- modelMark/src/main/java/com/wynne/knowledge/mark/home/BookActivity.java
+++ modelMark/src/main/java/com/wynne/knowledge/mark/home/BookActivity.java
@@ -1,5 +1,7 @@
 package com.wynne.knowledge.mark.home;
 
+import android.util.Log;
+
 import com.wynne.knowledge.base.base.BaseActivity;
 import com.wynne.knowledge.mark.R;
 
@@ -7,9 +9,14 @@ import com.wynne.knowledge.mark.R;
  * @author xxw
  */
 public class BookActivity extends BaseActivity {
+    private String ab = "ab";
+    private String cd = "cd";
+    private String abcd = ab+cd;
+
     @Override
     public void initView() {
-
+        Log.d("XXW", ab.toString());
+        Log.d("XXW", abcd.toString());
     }
 
     @Override
