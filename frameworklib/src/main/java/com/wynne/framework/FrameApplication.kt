fb0ce0af5592c23db9bf8eec13d29ffe8133f040
xiangxianwen <a664966415@gmail.com>
2019-05-24 23:32:20 +0800

    添加框架Module

---
+++ frameworklib/src/main/java/com/wynne/framework/FrameApplication.kt
@@ -0,0 +1,10 @@
+package com.wynne.framework
+
+import android.app.Application
+
+class FrameApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ frameworklib/src/main/java/com/wynne/framework/FrameWorkActivity.kt
@@ -0,0 +1,11 @@
+package com.wynne.framework
+
+import android.app.Activity
+import android.os.Bundle
+
+class FrameWorkActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
