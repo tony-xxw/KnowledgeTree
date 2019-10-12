5f1b5b8cb43742fffbbabb503ef1a1832e220881
xiangxianwen <a664966415@gmail.com>
2019-10-12 16:38:14 +0800

    navigation type

--- model_advanced/src/main/java/com/wynne/advanced/jetpack/navigation/LoginFragment.kt
+++ model_advanced/src/main/java/com/wynne/advanced/jetpack/navigation/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.wynne.advanced.jetpack.navigation
 
+import android.util.Log
 import com.wynne.advanced.R
 import com.wynne.knowledge.base.base.view.BaseFragment
 import kotlinx.android.synthetic.main.fragment_login_advanced.*
@@ -8,6 +9,7 @@ class LoginFragment : BaseFragment() {
     override fun getLayoutId(): Int = R.layout.fragment_login_advanced
 
     override fun initView() {
+        Log.d("XXW","argument : "+ arguments?.getString("name"))
         tvAccount.text = arguments?.getString("name")
     }
 
--- model_advanced/src/main/java/com/wynne/advanced/jetpack/navigation/WelComeFragment.kt
+++ model_advanced/src/main/java/com/wynne/advanced/jetpack/navigation/WelComeFragment.kt
@@ -10,23 +10,40 @@ import kotlinx.android.synthetic.main.fragment_welcome_advanced.*
 class WelComeFragment : BaseFragment() {
     override fun initView() {
         btnLogin.setOnClickListener {
+//            通过id跳转
             val bundle = Bundle()
             bundle.putString("name", "Wynne")
+            findNavController().navigate(R.id.login,bundle)
 
-            findNavController().navigate(R.id.login, bundle)
+//            通过safe Args传参
+//            val action = WelComeFragmentDirections.actionWelcomeToLogin()
+//            action.name="Wynne"
+//            findNavController().navigate(action)
+
+
+
         }
 
         btnRegister.setOnClickListener {
             val action = WelComeFragmentDirections.actionWelcomeToRegister()
             findNavController().navigate(action)
-
-
         }
 
         btnMain.setOnClickListener {
             startActivity(Intent(activity, JatPackNavigationMainActivity::class.java))
         }
 
+        btnWelCome.setOnClickListener {
+            //跳转嵌套导航
+            findNavController().navigate(R.id.nesting)
+        }
+
+        btnGlobal.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("name", "Wynne")
+            //通过全局action跳转
+            findNavController().navigate(R.id.action_global_login,bundle)
+        }
 
     }
 
