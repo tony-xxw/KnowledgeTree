b41a284fed11a9a1cbad4e60d7e89a064ed75e1d
tony-xxw <a664966415@gmail.com>
2019-12-03 23:03:55 +0800

    数据结构-链表 come on!

---
+++ model_math/src/main/java/com/wynne/math/linked/LinkedListActivity.kt
@@ -0,0 +1,37 @@
+package com.wynne.math.linked
+
+import android.view.View
+
+import com.wynne.knowledge.base.base.BaseActivity
+import com.wynne.math.R
+
+/**
+ * @author Wynne
+ */
+class LinkedListActivity : BaseActivity() {
+    override fun initView() {
+
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_base_math_linked_layout
+    }
+
+    fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnSingle -> {
+
+            }
+            R.id.btnDouble -> {
+
+            }
+            R.id.btnRecycle -> {
+
+            }
+            R.id.btnSame -> {
+
+            }
+        }
+
+    }
+}
