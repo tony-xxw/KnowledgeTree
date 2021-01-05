c3c67d1b67db94c946c3ed137f404fc7ceed6e79
tony-xxw <a664966415@gmail.com>
2021-01-06 00:21:50 +0800

    lambda表达式

--- model_other/src/main/java/com/wynne/other/grammar/Grammar6.kt
+++ model_other/src/main/java/com/wynne/other/grammar/Grammar6.kt
@@ -6,7 +6,16 @@ fun main() {
     chapter1()
     println()
     chapter2()
+    println()
+    chapter3()
+    println()
+    chapter4()
+    println()
+    chapter5()
+    getType<Int>()
+
 }
+
 
 fun chapter1() {
     println("6.1")
@@ -44,9 +53,10 @@ fun html(init: HTML.(Int) -> Unit): HTML {
     return html
 }
 
+data class Stu(val name: String, val age: Int, val sex: String, val score: Int)
+
 fun chapter2() {
     println("6.2")
-
     val list = arrayOf(1, 2, 3, 4)
     val newList = list.map { it * 2 }
     val zhangsan = Stu("zhangsan", 30, "m", 85)
@@ -55,7 +65,6 @@ fun chapter2() {
     val jack = Stu("jack", 13, "f", 67)
     val lisa = Stu("lisa", 49, "f", 91)
     val pan = Stu("pan", 34, "m", 23)
-    println()
     println("filter, count")
     val students = listOf<Stu>(zhangsan, lisi, shaw, jack, lisa, pan)
     val filterStu = students.filter { it.sex == "m" }
@@ -70,16 +79,120 @@ fun chapter2() {
     println("sumBy、fold、reduce")
     val sumBy = students.sumBy { it.score } / students.size
     val fold = students.fold(0) { accumulator, student -> accumulator + student.score }
-    val numbers = listOf(5, 2, 10, 4)
-
-
-//    val reduce = students.reduce { acc, stu -> acc.score = acc.score + stu.score }
+    val reduceOrigin = arrayOf(1, 2, 3, 4, 5)
+    val reduce = reduceOrigin.reduce { init, index -> init + index }
     println("sumBy $sumBy")
     println("fold $fold")
     println("reduce $reduce")
+    println("groupBy")
+    val groupBy = students.groupBy { it.sex }
+    println("groupBy  $groupBy")
+
+    //fla
+    val flattenTestList = arrayOf(arrayOf(lisa, lisa), arrayOf(pan, shaw, jack))
+    println("flattenTestList  $flattenTestList")
+    val flatten = flattenTestList.flatten()
+    println("flatten $flatten")
+    val flatMap = flattenTestList.flatMap { it.map { stu -> stu.name } }
+    println("flatMap $flatMap")
+
 
 }
 
-data class Stu(val name: String, val age: Int, val sex: String, val score: Int)
+
+fun chapter3() {
+    println("6.3")
+    println("6.3.1")
+    val list = listOf(1, 2, 3, 4, 5)
+    println("list $list")
+    val set = setOf(1, 1, 2, 2, 3, 3, 4, 4, 5, 5)
+    println("set $set")
+    val map = mapOf(1 to 1, 2 to 2, 2 to 3, 3 to 3)
+    println("map $map")
+    println("6.3.2")
+    val mutableListOf = mutableListOf(1, 2, 3, 4)
+    mutableListOf[0] = -1
+    println("mutable $mutableListOf")
+    var readList = listOf(1, 2, 3, 4)
+//    readList[0] = 1 false 不可变
+    //改变只读属性
+    readList = mutableListOf
+    println("readList $readList")
+    mutableListOf[0] = 10
+    println("readList $readList")
+
+}
+
+fun chapter4() {
+    println("6.4")
+    println("6.4.1")
+    val list = listOf(1, 2, 3)
+    list.filter {
+        println("filter $it")
+        it > 1
+    }.map {
+        println("map $it")
+        it * 2
+    }
+    println("6.4.2")
+    //当数据量很大 会有性能问题, 因为会先走filter，在走map,而且会产生一个临时对象
+    //利用序列提高效率, asSequence不会产生临时对象
+    // asSequence 是惰性求职, 会先有一个中间操作,在执行末端操作
+    list.asSequence().filter {
+        println("asSequence filter $it")
+        it > 1
+    }.map {
+        println("asSequence map $it")
+        it * 2
+    }.toList()
+    println("6.4.3")
+    var naturalNumList = generateSequence(0) { it + 1 }
+    println("naturalNumList ${naturalNumList.takeWhile { it <= 9 }.toList()}")
+}
 
 
+fun chapter5() {
+    println("6.5")
+    // 加入关键字Inline 减少开销
+    // 没有加入 Inline 关键字,编译为字节码后 反编译会通过反射来调用 增加开支
+    // 加入后 会直接copy过来,减少开支
+    // 使用场景: 以lambda表达式为参数的函数使用,减少开支
+    foo {
+        println("kotlin")
+    }
+
+    foo({
+        println("kotlin")
+    }, {
+        println("kotlin1")
+    })
+
+    localFoo {
+        return
+    }
+
+
+}
+
+inline fun foo(block: () -> Unit) {
+    println("before lock")
+    block()
+    println("after lock")
+}
+
+inline fun foo(block: () -> Unit, noinline block1: () -> Unit) {
+    println("before lock")
+    block()
+    block1()
+    println("after lock")
+}
+
+inline fun localFoo(block: () -> Unit) {
+    println("localFoo before lock")
+    block()
+    println("localFoo after lock")
+}
+
+inline fun <reified T> getType() {
+    println(T::class)
+}
