2e7c45930c0e480cb2a46ee14f760b73860df2d5
向鲜汶 <xiangxianwen@dianmi365.com>
2021-01-09 18:29:49 +0800

    #chapter 9 finishi

--- model_other/src/main/java/com/wynne/other/grammar/Chapter9.kt
+++ model_other/src/main/java/com/wynne/other/grammar/Chapter9.kt
@@ -10,6 +10,313 @@ fun main() {
     chapterObserver()
     chapterStrategy()
     chapterTemplate()
+    chapterIterator()
+    chapterChain()
+    chapterState()
+    chapterDecorate()
+}
+
+fun chapterDecorate() {
+    println("装饰模式")
+    val macBook = MacBookPro()
+    val process = ProcessUpgradeMacbookPro(macBook)
+    println(process.getCost())
+    println(process.getDesc())
+
+    Printer().run {
+        startDraw {
+            drawLine()
+        }
+        startDraw {
+            drawDottedLine()
+        }
+        startDraw {
+            drawStars()
+        }
+    }
+}
+
+class Printer {
+    fun drawLine() {
+        println("______")
+    }
+
+    fun drawDottedLine() {
+        println("-----")
+    }
+
+    fun drawStars() {
+        println("*******")
+    }
+}
+
+fun Printer.startDraw(decor: Printer.() -> Unit) {
+    println("++start")
+    this.decor()
+    println("== end")
+}
+
+interface MacBook {
+    fun getCost(): Int
+    fun getDesc(): String
+    fun getProDaTE(): String
+}
+
+class MacBookPro : MacBook {
+    override fun getCost(): Int = 1000
+
+    override fun getDesc(): String = "Macbook Pro"
+
+    override fun getProDaTE(): String = "Late 2011"
+}
+
+class ProcessUpgradeMacbookPro(val macbook: MacBook) : MacBook by macbook {
+    override fun getCost(): Int {
+        return macbook.getCost() + 219
+    }
+
+    override fun getDesc(): String {
+        return macbook.getDesc() + ", 1G Memory"
+    }
+
+}
+
+fun chapterState() {
+    println("状态模式")
+    val waterMachine = WaterMachine()
+    waterMachineOps(waterMachine, Moment.DRINKING_WARTER)
+    waterMachineOps(waterMachine, Moment.INSTANCE_NOODLES)
+    waterMachineOps(waterMachine, Moment.DRINKING_WARTER)
+    waterMachineOps(waterMachine, Moment.AFTER_WORK)
+}
+
+sealed class WaterMachinesState(open val machine: WaterMachine) {
+    fun turnHeating() {
+        if (this !is Heating) {
+            println("turn heating")
+            machine.state = machine.heating
+        } else {
+            println("the state is already headting mode")
+        }
+    }
+
+    fun turnCooling() {
+        if (this !is Cooling) {
+            println("turn Cooling")
+            machine.state = machine.cooling
+        } else {
+            println("the state is already Cooling mode")
+        }
+    }
+
+    fun turnOff() {
+        if (this !is Off) {
+            println("turn Off")
+            machine.state = machine.off
+        } else {
+            println("the state is already Off mode")
+        }
+    }
+
+}
+
+enum class Moment {
+    EARLY_MORNING,
+    DRINKING_WARTER,
+    INSTANCE_NOODLES,
+    AFTER_WORK
+}
+
+fun waterMachineOps(machine: WaterMachine, moment: Moment) {
+    when (moment) {
+        Moment.EARLY_MORNING,
+        Moment.DRINKING_WARTER -> when (machine.state) {
+            !is Cooling -> machine.turnCooling()
+        }
+        Moment.INSTANCE_NOODLES -> when (machine.state) {
+            !is Heating -> machine.turnHeating()
+        }
+        Moment.AFTER_WORK -> when (machine.state) {
+            !is Off -> machine.turnOff()
+        }
+        else -> Unit
+    }
+}
+
+class Off(override val machine: WaterMachine) : WaterMachinesState(machine)
+class Heating(override val machine: WaterMachine) : WaterMachinesState(machine)
+class Cooling(override val machine: WaterMachine) : WaterMachinesState(machine)
+
+class WaterMachine {
+    var state: WaterMachinesState
+    val off = Off(this)
+    val heating = Heating(this)
+    val cooling = Cooling(this)
+
+    init {
+        this.state = off
+    }
+
+    fun turnHeating() {
+        this.state.turnHeating()
+    }
+
+    fun turnCooling() {
+        this.state.turnCooling()
+    }
+
+    fun turnOff() {
+        this.state.turnOff()
+    }
+}
+
+fun chapterChain() {
+    println("责任链模式")
+    val groupLeader = GroupLeader(Preside(College(null)))
+    groupLeader.handleEvent(ApplyEvent(10, "buy a pen"))
+    groupLeader.handleEvent(ApplyEvent(200, "team building"))
+    groupLeader.handleEvent(ApplyEvent(600, "hold a debate match"))
+    groupLeader.handleEvent(ApplyEvent(1200, "annual meeting of the college"))
+
+    //Kotlin 偏函数实现
+    val groupLeader1 = {
+        val definerAt: (ApplyEvent) -> Boolean = { it.money <= 200 }
+        val handler: (ApplyEvent) -> Unit =
+                { println("Group Leader handled application: ${it.title}") }
+        PartialFunction(definerAt, handler)
+    }()
+
+    val president = {
+        val definerAt: (ApplyEvent) -> Boolean = { it.money <= 500 }
+        val handler: (ApplyEvent) -> Unit =
+                { println("President  handled application: ${it.title}") }
+        PartialFunction(definerAt, handler)
+    }()
+
+    val college = {
+        val definerAt: (ApplyEvent) -> Boolean = { true }
+        val handler: (ApplyEvent) -> Unit = {
+            when {
+                it.money > 1000 -> println("College: This application is refused")
+                else -> println("College  handled application: ${it.title}")
+            }
+        }
+        PartialFunction(definerAt, handler)
+    }()
+
+    val applyChain = groupLeader1 orElse president orElse college
+    applyChain(ApplyEvent(600, "hold a debate match"))
+}
+
+class PartialFunction<in P1, out R>(
+        private val definerAt: (P1) -> Boolean,
+        private val f: (P1) -> R
+) : (P1) -> R {
+    override fun invoke(p1: P1): R {
+        if (definerAt(p1)) {
+            return f(p1)
+        } else {
+            throw IllegalArgumentException("Value $p1 isn't supported by this.function")
+        }
+    }
+
+    fun isDefinedAt(p1: P1) = definerAt(p1)
+}
+
+infix fun <P1, R> PartialFunction<P1, R>.orElse(that: PartialFunction<P1, R>): PartialFunction<P1, R> {
+    return PartialFunction({ this.isDefinedAt(it) || that.isDefinedAt(it) }) {
+        when {
+            this.isDefinedAt(it) -> this(it)
+            else -> that(it)
+        }
+    }
+}
+
+data class ApplyEvent(val money: Int, val title: String)
+interface ApplyHandler {
+    val success: ApplyHandler?
+    fun handleEvent(event: ApplyEvent)
+}
+
+class GroupLeader(override val success: ApplyHandler?) : ApplyHandler {
+    override fun handleEvent(event: ApplyEvent) {
+        when {
+            event.money <= 100 -> println("Group leader handle application :${event.title}")
+            else -> when (success) {
+                is ApplyHandler -> success.handleEvent(event)
+                else -> println("Group leader: this application cannot be handle")
+            }
+        }
+    }
+}
+
+class Preside(override val success: ApplyHandler?) : ApplyHandler {
+    override fun handleEvent(event: ApplyEvent) {
+        when {
+            event.money <= 500 -> println("Preside leader handle application :${event.title}")
+            else -> when (success) {
+                is ApplyHandler -> success.handleEvent(event)
+                else -> println("Preside leader: this application cannot be handle")
+            }
+        }
+    }
+}
+
+
+class College(override val success: ApplyHandler?) : ApplyHandler {
+    override fun handleEvent(event: ApplyEvent) {
+        when {
+            event.money <= 1000 -> println("College leader handle application :${event.title}")
+            else -> when (success) {
+                is ApplyHandler -> success.handleEvent(event)
+                else -> println("College leader: this application cannot be handle")
+            }
+        }
+    }
+}
+
+fun chapterIterator() {
+    println("迭代器模式")
+    //迭代器模式
+    val bookcase = Bookcase(listOf(Book("Dive into Kotlin"), Book("Thinking in Java")))
+
+//    while (bookcase.hasNext()) {
+//        println("The Book Name is ${bookcase.next().name}")
+//    }
+    for (book in bookcase) {
+        println("The book name is ${book.name}")
+    }
+    println()
+    //重载
+    for (book in BookCase1(listOf(Book("Dive into Kotlin"), Book("Thinking in Java"))).iterator()) {
+        println("The book name is ${book.name}")
+    }
+
+
+}
+
+
+data class Book(val name: String)
+class BookCase1(val books: List<Book>) {
+    operator fun iterator(): Iterator<Book> = this.books.iterator()
+}
+
+class BookCase2(val books: List<Book>)
+
+operator fun BookCase1.iterator(): Iterator<Book> = books.iterator()
+operator fun BookCase2.iterator(): Iterator<Book> = object : Iterator<Book> {
+    val iterator = books.iterator()
+    override fun hasNext(): Boolean = iterator.hasNext()
+    override fun next() = iterator.next()
+}
+
+class Bookcase(books: List<Book>) : Iterator<Book> {
+    private val iterators: Iterator<Book> = books.iterator()
+
+    override fun hasNext(): Boolean = iterators.hasNext()
+
+    override fun next(): Book = iterators.next()
+
 }
 
 fun chapterTemplate() {
@@ -43,7 +350,7 @@ abstract class CivicCenterTask {
     abstract fun askForHelp()
 }
 
-fun pullSocialSecurity(){
+fun pullSocialSecurity() {
     println("ask for pulling ")
 }
 
@@ -337,6 +644,3 @@ object ObjcetComputerFactory {
         ComputerType.Server -> Service()
     }
 }
-
-
-
